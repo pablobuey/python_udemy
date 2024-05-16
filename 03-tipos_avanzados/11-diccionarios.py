11a8f4c67d43a7cbcc3e09f9b96de35ad1731233
pablobuey <pablobuey@gmail.com>
2024-05-16 10:00:53 +0200

    diccionarios

--- 03-tipos_avanzados/11-diccionarios.py
+++ 03-tipos_avanzados/11-diccionarios.py
@@ -20,3 +20,42 @@ if "lala" in punto:
 else:
     print("no lala in punto")
 
+
+print(punto.get("x"))
+print(punto.get("lala")) # devuelve None porque no existe
+
+print(punto.get("lala", 97)) # devuelve 97
+
+del punto["x"] # eliminas la clave valor x
+
+del(punto["y"]) # lo mismo pero con una función
+
+print(punto) # solo imprime el par de z
+
+punto["x"] = 25
+
+for valor in punto:
+    print(valor) # z x
+    #para acceder a los valores de z x:
+    print (valor, punto[valor]) # z 45 x 25
+
+for valor in punto.items():
+    print(valor)    
+    # ('z', 45)
+    # ('x', 25)
+
+for llave, valor in punto.items(): # desempaquetado
+    print(llave, valor)   
+    # z 45
+    # x 25
+
+usuarios = [
+    {"id": 1, "nombre": "Chanchito"},
+    {"id": 2, "nombre": "Feliz"},
+    {"id": 3, "nombre": "Nicolas"},
+    {"id": 4, "nombre": "Felipe"},
+]
+
+for usuario in usuarios:
+    print(usuario["nombre"])
+
